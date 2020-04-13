af5b9771d67cbb619f3ae6925419ee7539ad4964
rchild <rchild@mastercontrol.com>
2020-04-13 08:39:39 -0600

    Following the course

---
+++ src/app/heroes/heroes.component.shallow.spec.ts
@@ -0,0 +1,60 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeroesComponent } from './heroes.component';
+import { Test } from 'tslint';
+import { Component, EventEmitter, Input, NO_ERRORS_SCHEMA, Output } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { of } from 'rxjs';
+import { Hero } from '../hero';
+import { By } from '@angular/platform-browser';
+
+describe('HeroesComponent', () => {
+  let fixture: ComponentFixture<HeroesComponent>;
+  let mockHeroService;
+  let HEROES;
+
+  @Component({
+    selector: 'app-hero',
+    template: '<div></div>',
+  })
+  class FakeHeroComponent {
+    @Input() hero: Hero;
+    // @Output() delete = new EventEmitter();
+    //
+    // onDeleteClick($event): void {
+    //   $event.stopPropagation();
+    //   this.delete.next();
+    // }
+  }
+
+  beforeEach(() => {
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+    HEROES = [
+      {id:1, name: 'SpiderDude', strength: 8},
+      {id:2, name: 'Wonderful Woman', strength: 24},
+      {id:3, name: 'SuperDude', strength: 55}
+    ];
+    TestBed.configureTestingModule({
+      declarations:[
+        HeroesComponent,
+        FakeHeroComponent
+      ],
+      providers:[
+        {provide: HeroService, useValue: mockHeroService}
+      ],
+      imports:[],
+    });
+    fixture = TestBed.createComponent(HeroesComponent);
+  });
+
+  it('should set heroes correctly from the service', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    expect(fixture.componentInstance.heroes.length).toBe(3);
+  });
+
+  it('should create one li for each hero', () =>{
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
+  })
+});
